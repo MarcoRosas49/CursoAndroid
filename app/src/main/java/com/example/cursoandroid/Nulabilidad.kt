2cd60d6dc8f8ffe1d6a631079ae02a71875b18b1
Marco <marcorosas1008@gmail.com>
2024-03-17 23:41:20 -0500

    Exercise_1_2

---
+++ app/src/main/java/com/example/cursoandroid/Nulabilidad.kt
@@ -0,0 +1,10 @@
+package com.example.cursoandroid
+
+fun main(){
+
+    var name:String? = null
+
+    println(name?.get(3)) // si no es null entonces dame el valor en la posicion 3
+    println(name?.get(3) ?: "Es null wey") // -> pero si lo es retorna el mensaje "es null wey"
+}
+
