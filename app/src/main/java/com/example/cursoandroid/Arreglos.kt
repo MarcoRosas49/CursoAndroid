62092fa56736340fc392a41289ad6010d083a029
Marco <marcorosas1008@gmail.com>
2024-03-18 23:54:46 -0500

    Arrays

---
+++ app/src/main/java/com/example/cursoandroid/Arreglos.kt
@@ -0,0 +1,23 @@
+package com.example.cursoandroid
+
+import kotlin.reflect.typeOf
+
+fun main(){
+
+    val weekdays = arrayOf("lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo")
+
+    weekdays[0] = "otroDia"
+    println(weekdays[0])
+
+    for( pos in weekdays.indices){   //         en caso solo queramos la posición
+        println(weekdays[pos])
+    }
+
+    for ((pos,value) in weekdays.withIndex() ){     // en caso queramos la posición y el valor
+        println("Posición: $pos tiene el valor de: $value")
+    }
+
+    for (value in weekdays){                // en caso solo queramos el valor
+        println("El valor es: $value")
+    }
+}
